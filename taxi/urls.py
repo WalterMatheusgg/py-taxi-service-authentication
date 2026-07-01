33dc6ecb7059a9f47434c9d31ba9c12d822994ff
Walter_504 <157768009+WalterMatheusgg@users.noreply.github.com>
2026-07-01 21:39:21 +0000

    Fix reverse match errors in base.html and urls.py

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 
 from .views import (
     index,
@@ -23,12 +22,6 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
-    path("accounts/login/", auth_views.LoginView.as_view(
-        template_name="registration/login.html"
-    ), name="login"),
-    path("accounts/logout/", auth_views.LogoutView.as_view(
-        next_page="login"
-    ), name="logout"),
 ]
 
 app_name = "taxi"
